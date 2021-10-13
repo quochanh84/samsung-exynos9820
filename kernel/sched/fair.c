a66761a731a7362e8dddc5e78eb6432626fa6aee
Diep Quynh <remilia.1505@gmail.com>
2021-10-13 17:20:07 +0700

    sched: ems: Don't account RT bandwidth on indicating LBT tipping point
    RT tasks usually takes a huge amount of bandwidth. When accounted, the CPU's
    capacity is reduced, hence the overutilization tipping point is reached faster,
    thus disables EAS and triggers load balancing, which we want to avoid
    
    Let's not account RT bandwidth for LBT for now
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9183,6 +9183,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	unsigned long flags;
 
 	cpu_rq(cpu)->cpu_capacity_orig = capacity;
+	update_lbt_overutil(cpu, capacity);
 
 	capacity *= arch_scale_max_freq_capacity(sd, cpu);
 	capacity >>= SCHED_CAPACITY_SHIFT;
@@ -9210,8 +9211,6 @@ skip_unlock: __attribute__ ((unused));
 
 	if (!capacity)
 		capacity = 1;
-
-	update_lbt_overutil(cpu, capacity);
 
 	cpu_rq(cpu)->cpu_capacity = capacity;
 	sdg->sgc->capacity = capacity;
